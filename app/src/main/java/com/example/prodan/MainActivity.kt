95f6f77f438cac198a3a0e8286f29c443c2f1a96
DiegoPy <diegorscorpion@gmail.com>
2022-09-22 19:22:55 -0500

    Navigation with BottomNavBar completed

--- app/src/main/java/com/example/prodan/MainActivity.kt
+++ app/src/main/java/com/example/prodan/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.prodan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.prodan.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +14,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.bottomNavigationView.selectedItemId = R.id.homeFragment
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.homeFragment -> replaceFragment(HomeFragment())
+                R.id.favoritesFragment -> replaceFragment(FavoritesFragment())
+                R.id.formsFragment -> replaceFragment(FormsFragment())
+                R.id.userFragment -> replaceFragment(UserFragment())
+                else -> replaceFragment(ContactFragment())
+            }
+
+            true
+        }
+    }
+
+    fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.fragmentContainerView, fragment)
+        fragmentTransaction.commit()
     }
 }
