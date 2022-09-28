a207d9880a789d66b70008368385a4863c230e53
A00830698 <83973548+SantiM1@users.noreply.github.com>
2022-09-28 15:36:07 -0500

    Graphs
    Implementar RadarGraph

--- app/src/main/java/com/example/prodan/UserFragment.kt
+++ app/src/main/java/com/example/prodan/UserFragment.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.prodan.databinding.FragmentUserBinding
+import com.github.mikephil.charting.data.RadarData
+import com.github.mikephil.charting.data.RadarDataSet
+import com.github.mikephil.charting.data.RadarEntry
+import com.github.mikephil.charting.utils.ColorTemplate
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,16 +22,12 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class UserFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentUserBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -34,28 +35,38 @@ class UserFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user, container, false)
+        _binding =FragmentUserBinding.inflate(inflater, container, false)
+        setVarChart()
+        return binding.root
+
+    }
+
+    fun setVarChart() {
+
+        val entries: MutableList<RadarEntry> = ArrayList()
+        entries.add(RadarEntry(0f, 30f))
+        entries.add(RadarEntry(1f, 80f))
+        entries.add(RadarEntry(2f, 60f))
+        entries.add(RadarEntry(3f, 50f))
+        entries.add(RadarEntry(5f, 70f))
+        entries.add(RadarEntry(6f, 60f))
+
+        val set = RadarDataSet(entries, "BarDataSet")
+        var colorList = mutableListOf<Int>()
+        ColorTemplate.JOYFUL_COLORS.forEach {colorList.add(it)  }
+
+
+
+        // var colorList = mutableListOf<Int>(ColorTemplate.JOYFUL_COLORS.forEach {  })
+        set.setColors(colorList)
+        // set.setColors(ColorTemplate.COLORFUL_COLORS)
+
+        var data = RadarData(set)
+        binding.chart1.data = data
+        binding.chart1.animateY(2000)
+        //   binding.migrafica.invalidate()
 
 
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment UserFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            UserFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
