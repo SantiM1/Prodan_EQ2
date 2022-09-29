cb2728b3b332b02b69de0bfa82040c06ad1dc48c
A00830698 <83973548+SantiM1@users.noreply.github.com>
2022-09-29 18:29:31 -0500

    Add Edit Graphs functionality
    Permite editar grafos

--- app/src/main/java/com/example/prodan/user/UserFragment.kt
+++ app/src/main/java/com/example/prodan/user/UserFragment.kt
@@ -1,5 +1,6 @@
 package com.example.prodan
 
+import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,8 +8,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
+import androidx.navigation.fragment.findNavController
 import com.example.prodan.databinding.FragmentUserBinding
 import com.github.mikephil.charting.components.AxisBase
+import com.github.mikephil.charting.components.Description
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.RadarData
 import com.github.mikephil.charting.data.RadarDataSet
@@ -42,14 +45,36 @@ class UserFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding =FragmentUserBinding.inflate(inflater, container, false)
         setVarChart()
-        binding.nextBtn.visibility = View.GONE
 
         return binding.root
 
 
     }
 
-    fun setVarChart() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.nextBtn.visibility = View.GONE
+        binding.editBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_userFragment_to_editProfileFragment)
+        }
+    }
+    private fun setVarChart() {
+
+        val preferences = this.activity?.getPreferences(Context.MODE_PRIVATE)
+        val r1 = preferences?.getFloat("C1", 0f)
+        val r2 = preferences?.getFloat("C2", 0f)
+        val r3 = preferences?.getFloat("C3", 0f)
+        val r4 = preferences?.getFloat("C4", 0f)
+        val r5 = preferences?.getFloat("C5", 0f)
+        val name =preferences?.getString("PetName", "")
+        val description = preferences?.getString("Description", "")
+
+        if(name != "" && description != ""){
+            binding.PetName.text = name
+            binding.PetDescription.text = description
+        }
+
+
 
         val xvalues = ArrayList<String>()
         xvalues.apply{
@@ -63,11 +88,11 @@ class UserFragment : Fragment() {
 
 
         val entries: MutableList<RadarEntry> = ArrayList()
-        entries.add(RadarEntry(1f, "Bueno con niños"))
-        entries.add(RadarEntry(2f, "Sociable"))
-        entries.add(RadarEntry(3f, "Energético"))
-        entries.add(RadarEntry(4f, "Amigable"))
-        entries.add(RadarEntry(5f, "Travieso"))
+        r1?.let { RadarEntry(it, "Bueno con niños") }?.let { entries.add(it) }
+        r2?.let { RadarEntry(it, "Sociable") }?.let { entries.add(it) }
+        r3?.let { RadarEntry(it, "Energético") }?.let { entries.add(it) }
+        r4?.let { RadarEntry(it, "Amigable") }?.let { entries.add(it) }
+        r5?.let { RadarEntry(it, "Travieso") }?.let { entries.add(it) }
 
         val set = RadarDataSet(entries, "RadarDataSet")
         var colorList = mutableListOf<Int>()
@@ -76,8 +101,8 @@ class UserFragment : Fragment() {
 
 
         // var colorList = mutableListOf<Int>(ColorTemplate.JOYFUL_COLORS.forEach {  })
-        set.color = Color.rgb(10, 110, 180)
-        set.fillColor = Color.rgb(10, 110, 180);
+        set.color = Color.rgb(10, 120, 180)
+        set.fillColor = Color.rgb(52, 173, 207);
         set.setDrawFilled(true);
         set.fillAlpha = 180;
         set.lineWidth = 2f;
@@ -86,12 +111,17 @@ class UserFragment : Fragment() {
         // set.setColors(ColorTemplate.COLORFUL_COLORS)
 
         var data = RadarData(set)
-        binding.radarChart.data = data
-        binding.radarChart.animateY(2000)
-        binding.radarChart.description.isEnabled = false
-        binding.radarChart.legend.isEnabled = false
-        binding.radarChart.description.text = "Datos"
-        binding.radarChart.xAxis.valueFormatter = (myValueFormater(xvalues))
+        binding.apply {
+            radarChart.data = data
+            radarChart.animateY(2000)
+            radarChart.description.isEnabled = false
+            radarChart.legend.isEnabled = false
+            radarChart.description.text = "Datos"
+            radarChart.xAxis.valueFormatter = (myValueFormater(xvalues))
+            radarChart.yAxis.axisMinimum = 0f
+            radarChart.yAxis.axisMaximum = 5f
+            radarChart.yAxis.isEnabled = false
+        }
 
             //   binding.migrafica.invalidate()
 
@@ -112,4 +142,6 @@ class myValueFormater(private val xvalues : ArrayList<String>) : ValueFormatter(
             return ("").toString()
         }
     }
+
 }
+
