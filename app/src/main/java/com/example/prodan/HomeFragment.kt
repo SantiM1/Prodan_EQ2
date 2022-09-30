43565a4f1c4ecf0fa7c57c1569ebd9d2580636af
RicardoHdzR <A00831818@itesm.mx>
2022-09-30 16:17:16 -0500

    Implementacion en Home Fragment

--- app/src/main/java/com/example/prodan/HomeFragment.kt
+++ app/src/main/java/com/example/prodan/HomeFragment.kt
@@ -6,7 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.prodan.data.petList
 import com.example.prodan.databinding.FragmentHomeBinding
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,20 +24,25 @@ private const val ARG_PARAM2 = "param2"
  */
 class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
-
-    private var _binding : FragmentHomeBinding? = null
+    private var param1: String? = null
+    private var param2: String? = null
+    private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        _binding = FragmentHomeBinding.inflate(inflater,container,false)
+        // Inflate the layout for this fragment
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
         return binding.root
     }
@@ -41,34 +50,37 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonList.setOnClickListener {
-            Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_fragment_list)
-
+        // Creando instancia de adaptador
+        val adapter = adapter(requireActivity(), petList){
+            val bundle = Bundle()
+            bundle.putParcelable("pet",it)
+            //findNavController().navigate(R.id.action_homeFragment_to_detailFragment)
         }
+
+        binding.rvpet.adapter = adapter
+        binding.rvpet.layoutManager =
+            GridLayoutManager(requireActivity(),
+                2, RecyclerView.VERTICAL, false)
+
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
-    companion object {
+    /*companion object {
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
+         * @return A new instance of fragment fragment_list.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
+            fragment_list().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
             }
-    }
+    }*/
 }
