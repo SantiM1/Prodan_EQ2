4aab6c3521f5ef36a0e84884e8226e68ca1bea26
Eduardo Valerin <a00830774@itesm.mx>
2022-10-10 17:41:14 -0500

    Forms completed
    El formulario se puede ver en la aplicación haciendo uso de un WebView

--- app/src/main/java/com/example/prodan/FormsFragment.kt
+++ app/src/main/java/com/example/prodan/FormsFragment.kt
@@ -1,10 +1,12 @@
 package com.example.prodan
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import com.example.prodan.databinding.FragmentFormsBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,10 +19,14 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class FormsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentFormsBinding? = null
+    private val binding get() = _binding!!
 
+    // TODO: Rename and change types of parameters
+    // private var param1: String? = null
+    // private var param2: String? = null
+
+    /*
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -28,13 +34,54 @@ class FormsFragment : Fragment() {
             param2 = it.getString(ARG_PARAM2)
         }
     }
+     */
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forms, container, false)
+        _binding = FragmentFormsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val myWebView: WebView = view.findViewById(R.id.webviewForms)
+        myWebView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView,
+                url: String
+            ): Boolean {
+                view.loadUrl(url)
+                return true
+            }
+        }
+
+        val webSetting: WebSettings = myWebView.settings
+        webSetting.javaScriptEnabled = true
+        myWebView.webViewClient = WebViewClient()
+
+        myWebView.canGoBack()
+        myWebView.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_BACK
+
+                && event.action == MotionEvent.ACTION_UP
+                && myWebView.canGoBack()
+            ) {
+                myWebView.goBack()
+                return@OnKeyListener true
+            }
+            false
+        })
+
+
+        myWebView.loadUrl("https://docs.google.com/forms/d/e/1FAIpQLSc9Nj7H1W5kaIEvD2gPP2nyQ1G4iKndw90BJWHs8_Maz6tADw/viewform")
+        myWebView.settings.javaScriptEnabled = true
+        myWebView.settings.allowContentAccess = true
+        myWebView.settings.domStorageEnabled = true
+        myWebView.settings.useWideViewPort = true
     }
 
     companion object {
